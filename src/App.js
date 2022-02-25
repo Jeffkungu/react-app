35d411d5f52d17d01a10035232fb2c434aae1985
Jeffkungu <jeffkungu@gmail.com>
2022-02-25 13:10:30 +0300

    [feature] initialises project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+
+function App () {
+  return (
+    <fragment>
+      <Header />
+      <Meals />
+    </fragment>
+  )
+}
+
+export default App
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from './MealsSummary'
+import AvailableMeals from './AvailableMeals'
+
+const Meals = () => {
+  return (
+    <fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </fragment>
+  )
+}
+
+export default Meals
